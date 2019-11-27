ef422ed6daacc5ac74e9ef8c4292273e3b034cab
Sebastiaan de Schaetzen <sebastiaan.de.schaetzen@gmail.com>
2019-11-27 20:41:03 +0100

    Add unit tests

---
+++ src/test/java/be/seeseemelk/easydsp/SampleTest.java
@@ -0,0 +1,15 @@
+package be.seeseemelk.easydsp;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest
+{
+	@Test
+	public void sampleTest()
+	{
+		assertThat(true, equalTo(true));
+	}
+}
